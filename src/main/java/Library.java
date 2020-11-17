efa4a4227b89e8c335696f360e65f66fba6c8c15
Your Name <mmuzasantuna@gmail.com>
2020-11-17 20:40:54 +0000

    working with HashMap

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Library {
     private ArrayList<Book> collection;
     private int capacity;
+    private HashMap<Book, String> fictionCollection;
 
     public Library(int capacity) {
         this.collection = new ArrayList<>();
         this.capacity = capacity;
+        this.fictionCollection = new HashMap<>();
     }
 
     public int countBooksInCollection() {
@@ -21,5 +24,9 @@ public class Library {
 
     public Book removeBook() {
         return this.collection.remove(0);
+    }
+
+    public void addFictionBooks() {
+        fictionCollection.put()
     }
 }
